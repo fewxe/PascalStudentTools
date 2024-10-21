d7e9dd1c2bdc814d81d4bc1a6cc8473b2ccd5cb5
marsel <garifyllinmarsel197688@gmail.com>
2024-10-21 10:47:33 +0400

    .net moment)

--- PascalToolsFirstYear.pas
+++ PascalToolsFirstYear.pas
@@ -4,85 +4,23 @@ interface
 
 uses crt;
 
-function ReadArray(arr: array of integer): array of integer;
+function ReadArray<T>(arr: array of T): array of T;
 
-function ReadArray(
-  arr: array of integer;
+function ReadArray<T>(
+  arr: array of T;
   outX: integer;
   outY: integer;
   inputX: integer;
-  inputY: integer): array of integer;
-  
-  
-function ReadArray(arr: array of real): array of real;
+  inputY: integer): array of T;
 
-function ReadArray(
-  arr: array of real;
+function ReadArray<T>(arr: array [,] of T): array [,] of T;
+
+function ReadArray<T>(
+  arr: array [,] of T;
   outX: integer;
   outY: integer;
   inputX: integer;
-  inputY: integer): array of real;
-  
-
-function ReadArray(arr: array of string): array of string;
-
-function ReadArray(
-  arr: array of string;
-  outX: integer;
-  outY: integer;
-  inputX: integer;
-  inputY: integer): array of string;
-
-
-function ReadArray(arr: array of char): array of char;
-
-function ReadArray(
-  arr: array of char;
-  outX: integer;
-  outY: integer;
-  inputX: integer;
-  inputY: integer): array of char;
-
-
-function ReadArray(arr: array [,] of integer): array [,] of integer;
-
-function ReadArray(
-  arr: array [,] of integer;
-  outX: integer;
-  outY: integer;
-  inputX: integer;
-  inputY: integer): array [,] of integer;
-
-
-function ReadArray(arr: array [,] of real): array [,] of real;
-
-function ReadArray(
-  arr: array [,] of real;
-  outX: integer;
-  outY: integer;
-  inputX: integer;
-  inputY: integer): array [,] of real;
-
-
-function ReadArray(arr: array [,] of string): array [,] of string;
-
-function ReadArray(
-  arr: array [,] of string;
-  outX: integer;
-  outY: integer;
-  inputX: integer;
-  inputY: integer): array [,] of string;
-
-
-function ReadArray(arr: array [,] of char): array [,] of char;
-
-function ReadArray(
-  arr: array [,] of char;
-  outX: integer;
-  outY: integer;
-  inputX: integer;
-  inputY: integer): array [,] of char;
-
+  inputY: integer): array [,] of T;
 
 procedure PrintArray<T>(arr: array of T);
 
@@ -91,13 +29,13 @@ procedure PrintArray<T>(
   x: integer; 
   y: integer);
 
-
 procedure PrintArray<T>(arr: array [,] of T);
 
 procedure PrintArray<T>(
   arr: array [,] of T; 
   x: integer; 
   y: integer);
+
 
 
 implementation
@@ -124,6 +62,7 @@ begin
   crt.GoToXY(1, crt.WhereY() + 1);
 end;
 
+
 procedure PrintArray<T>(
   arr: array of T; 
   x: integer; 
@@ -132,12 +71,14 @@ begin
   PrintOneDimensionalArray(arr, x, y);
 end;
 
+
 procedure PrintArray<T>(arr: array of T);
 begin
   var x: integer := crt.WhereX();
   var y: integer := crt.WhereY();
   PrintArray(arr, x, y);
 end;
+
 
 procedure PrintArray<T>(
   arr: array [,] of T; 
@@ -152,6 +93,7 @@ begin
   GoToXY(1, y);
 end;
 
+
 procedure PrintArray<T>(arr: array [,] of T);
 begin
   var x: integer := crt.WhereX();
@@ -160,16 +102,14 @@ begin
 end;
 
 
-
-// понеслась
-function ReadOneDimensionalArray(
-  arr: array of integer;
+function ReadOneDimensionalArray<T>(
+  arr: array of T;
   outX: integer;
   outY: integer;
   inputX: integer;
-  inputY: integer): array of integer;
+  inputY: integer): array of T;
 begin
-  var arr2: array of integer := new integer[arr.Length];
+  var arr2: array of T := new T[arr.Length];
   arr.CopyTo(arr2, 0);
   var s: string := '';
   s += '[';
@@ -183,7 +123,7 @@ begin
     s += str;
     if (i <> arr2.Length - 1) then 
       s += ', ';
-    arr2[i] := integer.Parse(str);
+    arr2[i] := T(Convert.ChangeType(str, typeof(T)));
     GoToXY(outX, outY);
     write(s);
   end;
@@ -195,120 +135,18 @@ begin
 end;
 
 
-function ReadOneDimensionalArray(
-  arr: array of real;
+function ReadArray<T>(
+  arr: array of T;
   outX: integer;
   outY: integer;
   inputX: integer;
-  inputY: integer): array of real;
-begin
-  var arr2: array of real := new real[arr.Length];
-  arr.CopyTo(arr2, 0);
-  var s: string := '';
-  GoToXY(outX, outY);
-  s += '[';
-  for var i: integer := 0 to arr2.Length - 1 do
-  begin
-    GoToXY(inputX, inputY);
-    Write('input: ');
-    var str: string := ReadString();
-    crt.GotoXY(1, WhereY() - 1);
-    crt.ClearLine();
-    s += str;
-    if (i <> arr2.Length - 1) then 
-      s += ', ';
-    arr2[i] := real.Parse(str);
-    GoToXY(outX, outY);
-    write(s);
-  end;
-  s += ']';
-  GoTOXY(outX, outY);
-  Write(s);
-  GoToXY(1, WhereY() + 1);
-  Result := arr2;
-end;
-
-
-function ReadOneDimensionalArray(
-  arr: array of string;
-  outX: integer;
-  outY: integer;
-  inputX: integer;
-  inputY: integer): array of string;
-begin
-  var arr2: array of string := new string[arr.Length];
-  arr.CopyTo(arr2, 0);
-  var s: string := '';
-  GoToXY(outX, outY);
-  s += '[';
-  for var i: integer := 0 to arr2.Length - 1 do
-  begin
-    GoToXY(inputX, inputY);
-    Write('input: ');
-    var str: string := ReadString();
-    crt.GotoXY(1, WhereY() - 1);
-    crt.ClearLine();
-    s += str;
-    if (i <> arr2.Length - 1) then 
-      s += ', ';
-    arr2[i] := str;
-    GoToXY(outX, outY);
-    write(s);
-  end;
-  s += ']';
-  GoTOXY(outX, outY);
-  Write(s);
-  GoToXY(1, WhereY() + 1);
-  Result := arr2;
-end;
-
-
-function ReadOneDimensionalArray(
-  arr: array of char;
-  outX: integer;
-  outY: integer;
-  inputX: integer;
-  inputY: integer): array of char;
-begin
-  var arr2: array of char := new char[arr.Length];
-  arr.CopyTo(arr2, 0);
-  var s: string := '';
-  GoToXY(outX, outY);
-  s += '[';
-  for var i: integer := 0 to arr2.Length - 1 do
-  begin
-    GoToXY(inputX, inputY);
-    Write('input: ');    
-    var str: string := ReadString();
-    crt.GotoXY(1, WhereY() - 1);
-    crt.ClearLine();
-    s += str;
-    if (i <> arr2.Length - 1) then 
-      s += ', ';
-    arr2[i] := char.Parse(str);
-    GoToXY(outX, outY);
-    write(s);
-  end;
-  s += ']';
-  GoTOXY(outX, outY);
-  Write(s);
-  GoToXY(1, WhereY() + 1);
-  Result := arr2;
-end;
-
-
-function ReadArray(
-  arr: array of integer;
-  outX: integer;
-  outY: integer;
-  inputX: integer;
-  inputY: integer): array of integer;
+  inputY: integer): array of T;
 begin
   Result := ReadOneDimensionalArray(arr, outX, outY, inputX, inputY)
 end;
 
 
-function ReadArray(arr: array of integer): array of integer;
+function ReadArray<T>(arr: array of T): array of T;
 begin
   var outX: integer := crt.WhereX();
   var outY: integer := crt.WhereY();
@@ -318,141 +156,17 @@ begin
 end;
 
 
-function ReadArray(
-  arr: array of real;
+function ReadArray<T>(
+  arr: array [,] of T;
   outX: integer;
   outY: integer;
   inputX: integer;
-  inputY: integer): array of real;
+  inputY: integer): array [,] of T;
 begin
-  Result := ReadOneDimensionalArray(arr, outX, outY, inputX, inputY)
-end;
-
-function ReadArray(arr: array of real): array of real;
-begin
-  var outX: integer := crt.WhereX();
-  var outY: integer := crt.WhereY();
-  var inputX: integer := Round(crt.WindowWidth() / 2);
-  var inputY: integer := crt.WindowHeight - 2;
-  Result := ReadArray(arr, outX, outY, inputX, inputY);
-end;
-
-
-
-function ReadArray(
-  arr: array of string;
-  outX: integer;
-  outY: integer;
-  inputX: integer;
-  inputY: integer): array of string;
-begin
-  Result := ReadOneDimensionalArray(arr, outX, outY, inputX, inputY)
-end;
-
-function ReadArray(arr: array of string): array of string;
-begin
-  var outX: integer := crt.WhereX();
-  var outY: integer := crt.WhereY();
-  var inputX: integer := Round(crt.WindowWidth() / 2);
-  var inputY: integer := crt.WindowHeight - 2;
-  Result := ReadArray(arr, outX, outY, inputX, inputY);
-end;
-
-
-function ReadArray(
-  arr: array of char;
-  outX: integer;
-  outY: integer;
-  inputX: integer;
-  inputY: integer): array of char;
-begin
-  Result := ReadOneDimensionalArray(arr, outX, outY, inputX, inputY)
-end;
-
-function ReadArray(arr: array of char): array of char;
-begin
-  var outX: integer := crt.WhereX();
-  var outY: integer := crt.WhereY();
-  var inputX: integer := Round(crt.WindowWidth() / 2);
-  var inputY: integer := crt.WindowHeight - 2;
-  Result := ReadArray(arr, outX, outY, inputX, inputY);
-end;
-
-
-
-function ReadArray(
-  arr: array [,] of integer;
-  outX: integer;
-  outY: integer;
-  inputX: integer;
-  inputY: integer): array [,] of integer;
-begin
-  var arr2: array [,] of integer := new integer[arr.RowCount(), arr.ColCount()];
+  var arr2: array [,] of T := new T[arr.RowCount(), arr.ColCount()];
   for var i: integer := 0 to arr.RowCount() - 1 do
   begin
-    var arr3: array of integer := ReadOneDimensionalArray(arr.Row(i), outX, outY, inputX, inputY);
-    for var j: integer := 0 to arr3.Length - 1 do
-    begin
-      arr2[i, j] := arr3[j];
-    end;
-    outY += 1;
-  end;
-  Result := arr2;
-end;
-
-function ReadArray(arr: array [,] of integer): array [,] of integer;
-begin
-  var outX: integer := crt.WhereX();
-  var outY: integer := crt.WhereY();
-  var inputX: integer := Round(crt.WindowWidth() / 2);
-  var inputY: integer := crt.WindowHeight - 2;
-  Result := ReadArray(arr, outX, outY, inputX, inputY);
-end;
-
-
-
-function ReadArray(
-  arr: array [,] of real;
-  outX: integer;
-  outY: integer;
-  inputX: integer;
-  inputY: integer): array [,] of real;
-begin
-  var arr2: array [,] of real := new real[arr.RowCount(), arr.ColCount()];
-  for var i: integer := 0 to arr.RowCount() - 1 do
-  begin
-    var arr3: array of real := ReadOneDimensionalArray(arr.Row(i), outX, outY, inputX, inputY);
-    for var j: integer := 0 to arr3.Length - 1 do
-    begin
-      arr2[i, j] := arr3[j];
-    end;
-    outY += 1;
-  end;
-  Result := arr2;
-end;
-
-function ReadArray(arr: array [,] of real): array [,] of real;
-begin
-  var outX: integer := crt.WhereX();
-  var outY: integer := crt.WhereY();
-  var inputX: integer := Round(crt.WindowWidth() / 2);
-  var inputY: integer := crt.WindowHeight - 2;
-  Result := ReadArray(arr, outX, outY, inputX, inputY);
-end;
-
-
-
-function ReadArray(
-  arr: array [,] of string;
-  outX: integer;
-  outY: integer;
-  inputX: integer;
-  inputY: integer): array [,] of string;
-begin
-  var arr2: array [,] of string := new string[arr.RowCount(), arr.ColCount()];
-  for var i: integer := 0 to arr.RowCount() - 1 do
-  begin
-    var arr3: array of string := ReadOneDimensionalArray(arr.Row(i), outX, outY, inputX, inputY);
+    var arr3: array of T := ReadOneDimensionalArray(arr.Row(i), outX, outY, inputX, inputY);
     for var j: integer := 0 to arr3.Length - 1 do
     begin
       arr2[i, j] := arr3[j];
@@ -463,38 +177,7 @@ begin
 end;
 
 
-function ReadArray(arr: array [,] of string): array [,] of string;
-begin
-  var outX: integer := crt.WhereX();
-  var outY: integer := crt.WhereY();
-  var inputX: integer := Round(crt.WindowWidth() / 2);
-  var inputY: integer := crt.WindowHeight - 2;
-  Result := ReadArray(arr, outX, outY, inputX, inputY);
-end;
-
-
-function ReadArray(
-  arr: array [,] of char;
-  outX: integer;
-  outY: integer;
-  inputX: integer;
-  inputY: integer): array [,] of char;
-begin
-  var arr2: array [,] of char := new char[arr.RowCount(), arr.ColCount()];
-  for var i: integer := 0 to arr.RowCount() - 1 do
-  begin
-    var arr3: array of char := ReadOneDimensionalArray(arr.Row(i), outX, outY, inputX, inputY);
-    for var j: integer := 0 to arr3.Length - 1 do
-    begin
-      arr2[i, j] := arr3[j];
-    end;
-    outY += 1;
-  end;
-  Result := arr2;
-end;
-
-
-function ReadArray(arr: array [,] of char): array [,] of char;
+function ReadArray<T>(arr: array [,] of T): array [,] of T;
 begin
   var outX: integer := crt.WhereX();
   var outY: integer := crt.WhereY();
