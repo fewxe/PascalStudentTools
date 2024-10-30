ad47a8e3595bfd65e6a6c7910bbebd9d31273716
marsel <garifyllinmarsel197688@gmail.com>
2024-10-31 00:07:35 +0400

    add class for working with complex numbers

--- PascalToolsFirstYear.pas
+++ PascalToolsFirstYear.pas
@@ -3,16 +3,17 @@
 interface
 
 uses UArrayTools;
+uses UComplexTools;
 
 
 type
   ArrayTools = class
-      
+    
     public static procedure PrintArray<T>(
       arr: array of T; 
       x: integer; 
       y: integer);
-      
+    
     public static procedure PrintArray<T>(arr: array of T);
     
     public static procedure PrintArray<T>(
@@ -39,59 +40,152 @@ type
       inputY: integer): array [,] of T;
     
     public static function ReadArray<T>(arr: array [,] of T): array [,] of T;
-end;
+  end;
 
+type
+  Complex = class(TComplex)
+    public re, im: real;
+    constructor Create(re, im: real);
+    static function operator+(a, b: Complex): Complex;
+    static function operator-(a, b: Complex): Complex;
+    static function operator*(a, b: Complex): Complex;
+    static function operator=(a, b: Complex): boolean;
+    static function operator/(a, b: Complex): Complex;
+    public function Abs(): real;
+    public function Prod(a: real): Complex;
+    public function Conjugate(): Complex;
+    public function Inv(): Complex;
+    public function ToString(): string;
+    static function operator explicit(a: Complex): string;
+    static function operator implicit(r: real): Complex;
+  end;
 
 implementation
-uses UArrayTools;
 
-  class procedure ArrayTools.PrintArray<T>(arr: array of T);
-  begin
-    UArrayTools.TArrayTools.PrintArray(arr);
-  end;
-  
-  class procedure ArrayTools.PrintArray<T>(arr: array of T; x: integer; y: integer);
-  begin
-    UArrayTools.TArrayTools.PrintArray(arr, x, y);
-  end;
-  
-  class procedure ArrayTools.PrintArray<T>(arr: array [,] of T);
-  begin
-    UArrayTools.TArrayTools.PrintArray(arr);
-  end;
-  
-  class procedure ArrayTools.PrintArray<T>(arr: array [,] of T; x: integer; y: integer);
-  begin
-    UArrayTools.TArrayTools.PrintArray(arr, x, y);
-  end;
-  
-  class function ArrayTools.ReadArray<T>(
+uses UArrayTools;
+uses UComplexTools;
+
+class procedure ArrayTools.PrintArray<T>(arr: array of T);
+begin
+  UArrayTools.TArrayTools.PrintArray(arr);
+end;
+
+class procedure ArrayTools.PrintArray<T>(arr: array of T; x: integer; y: integer);
+begin
+  UArrayTools.TArrayTools.PrintArray(arr, x, y);
+end;
+
+class procedure ArrayTools.PrintArray<T>(arr: array [,] of T);
+begin
+  UArrayTools.TArrayTools.PrintArray(arr);
+end;
+
+class procedure ArrayTools.PrintArray<T>(arr: array [,] of T; x: integer; y: integer);
+begin
+  UArrayTools.TArrayTools.PrintArray(arr, x, y);
+end;
+
+class function ArrayTools.ReadArray<T>(
       arr: array of T;
       outX: integer;
       outY: integer;
       inputX: integer;
       inputY: integer): array of T;
-   begin
-     Result := UArrayTools.TArrayTools.ReadArray(arr, outX, outY, inputX, inputY);
-   end;
-   
-   class function ArrayTools.ReadArray<T>(
+begin
+  Result := UArrayTools.TArrayTools.ReadArray(arr, outX, outY, inputX, inputY);
+end;
+
+class function ArrayTools.ReadArray<T>(
       arr: array [,] of T;
       outX: integer;
       outY: integer;
       inputX: integer;
       inputY: integer): array [,] of T;
-   begin
-     Result := UArrayTools.TArrayTools.ReadArray(arr, outX, outY, inputX, inputY);
-   end;
-   
-   class function ArrayTools.ReadArray<T>(arr: array of T): array of T;
-   begin
-     Result := UArrayTools.TArrayTools.ReadArray(arr);
-   end;
-   
-   class function ArrayTools.ReadArray<T>(arr: array [,] of T): array [,] of T;
-   begin
-     Result := UArrayTools.TArrayTools.ReadArray(arr);
-   end;
+begin
+  Result := UArrayTools.TArrayTools.ReadArray(arr, outX, outY, inputX, inputY);
+end;
+
+class function ArrayTools.ReadArray<T>(arr: array of T): array of T;
+begin
+  Result := UArrayTools.TArrayTools.ReadArray(arr);
+end;
+
+class function ArrayTools.ReadArray<T>(arr: array [,] of T): array [,] of T;
+begin
+  Result := UArrayTools.TArrayTools.ReadArray(arr);
+end;
+
+
+constructor Complex.Create(re, im: real);
+begin
+  inherited
+end;
+
+static function Complex.operator+(a, b: Complex): Complex;
+begin
+  var t: TComplex := TComplex(a) + TComplex(b);
+  Result := new Complex(t.re, t.im);
+end;
+
+static function Complex.operator-(a, b: Complex): Complex;
+begin
+  var t: TComplex := TComplex(a) - TComplex(b);
+  Result := new Complex(t.re, t.im);
+end;
+
+static function Complex.operator*(a, b: Complex): Complex;
+begin
+  var t: TComplex := TComplex(a) * TComplex(b);
+  Result := new Complex(t.re, t.im);
+end;
+
+static function Complex.operator=(a, b: Complex): boolean;
+begin
+  Result := TComplex(a) = TComplex(b);
+end;
+
+static function Complex.operator/(a, b: Complex): Complex;
+begin
+  var t: TComplex := TComplex(a) / TComplex(b);
+  Result := new Complex(t.re, t.im);
+end;
+
+function Complex.Abs(): real;
+begin
+  Result := inherited Abs();
+end;
+
+function Complex.Prod(a: real): Complex;
+begin
+  var t: TComplex := inherited Prod(a);
+  Result := new Complex(t.re, t.im);
+end;
+
+function Complex.Conjugate(): Complex;
+begin
+  var t: TComplex := inherited Conjugate();
+  Result := new Complex(t.re, t.im);
+end;
+
+function Complex.Inv(): Complex;
+begin
+  var t: TComplex := inherited Inv();
+  Result := new Complex(t.re, t.im);
+end;
+
+function Complex.ToString(): string;
+begin
+  Result := inherited 
+end;
+
+static function Complex.operator explicit(a: Complex): string;
+begin
+  Result := a.ToString();
+end;
+
+static function Complex.operator implicit(r: real): Complex;
+begin
+  Result := new Complex(r, 0);
+end; 
+
 end.
---
+++ UComplexTools.pas
@@ -0,0 +1,77 @@
+﻿unit UComplexTools;
+
+
+type 
+  TComplex = class
+    re, im: real;
+    
+    constructor Create(re, im: real);
+    begin
+      Self.re := re;
+      Self.im := im;
+    end;
+    
+    static function operator+(a, b: TComplex): TComplex;
+    begin
+      Result := new TComplex(a.re + b.re, a.im + b.im);
+    end;
+    
+    static function operator-(a, b: TComplex): TComplex;
+    begin
+      Result := new TComplex(a.re - b.re, a.im - b.im);
+    end;
+    
+    static function operator*(a, b: TComplex): TComplex;
+    begin
+      Result := new TComplex((a.re*b.re-a.im*b.im), (a.re*b.im+a.im*b.re));
+    end;
+    
+    static function operator=(a,b: TComplex): boolean;
+    begin
+      Result := (a.re = b.re) and (a.im = b.im);
+    end;
+    
+    public function Abs(): real;
+    begin
+      Result := Sqrt(Sqr(self.re)+Sqr(self.im));
+    end;
+    
+    public function Prod(a: real): TComplex;
+    begin
+      Result := new TComplex(a*self.re, a*self.im);
+    end;
+    
+    public function Conjugate(): TComplex;
+    begin
+      Result := new TComplex(self.re, -self.im)
+    end;
+    
+    public function Inv(): TComplex;
+    begin
+      var q: real := Sqr(self.re) + Sqr(self.im);
+      if (q = 0) then 
+        raise new Exception('Division by zero');
+      Result := new TComplex(self.re/q, self.im/q);
+    end;
+    
+    static function operator/(a, b: TComplex): TComplex;
+    begin
+      if (b.re = 0) and (b.im = 0) then
+        raise new Exception('Division by zero');
+      Result := a * b.Conjugate().Inv();
+    end;
+    
+    public function ToString(): string;
+    begin
+      var str: string;
+      if (self.re <> 0) then str += self.re.ToString();
+      
+      if (self.im <> 0) then
+        if (self.im > 0) then
+          str += '+' + PABCSystem.Abs(self.im).ToString() + 'i' 
+        else
+          str += '-' + PABCSystem.Abs(self.im).ToString() + 'i';
+      Result := str;
+    end;
+  end;
+end.
